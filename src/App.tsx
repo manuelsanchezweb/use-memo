677a93b4f111757baa95262288efbb0552342340
manuelsanchez2 <manusansan22@gmail.com>
2023-10-01 09:17:41 +0200

    Fix pipeline 😜

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Case1 from './components/Case1'
-import Case2 from './components/Case2'
+// import Case1 from './components/Case1'
+// import Case2 from './components/Case2'
 import Case3 from './components/Case3'
 
 function App() {
